74a6e63c64d10427d8786258da7af100708df24b
Floess, Constantin <cfloess@lcs-mba3343.lcs.on.ca>
2017-11-14 11:03:09 -0500

    triangle wit var

--- 50 Blank Canvases.playground/Pages/Canvas 5.xcplaygroundpage/Contents.swift
+++ 50 Blank Canvases.playground/Pages/Canvas 5.xcplaygroundpage/Contents.swift
@@ -9,14 +9,14 @@ let canvas = Canvas(width: 400, height: 300)
 
 var x : Int
 
-x = 200
-canvas.drawLine(fromX: 100, fromY: 100, toX: 200, toY: 100
+x = 300
+canvas.drawLine(fromX: 0, fromY: 0, toX: x, toY: 0
 )
-canvas.translate(byX: 200, byY: 100)
+canvas.translate(byX: x, byY: 0)
 canvas.rotate(by: 120)
-canvas.drawLine(fromX: 0, fromY: 0, toX: 100, toY: 0)
-canvas.translate(byX: 100, byY: 0)
+canvas.drawLine(fromX: 0, fromY: 0, toX: x, toY: 0)
+canvas.translate(byX: x, byY: 0)
 canvas.rotate(by: 120)
-canvas.drawLine(fromX: 0, fromY: 0, toX: 100, toY: 0)
+canvas.drawLine(fromX: 0, fromY: 0, toX: x, toY: 0)
 // Don't remove the code below
 PlaygroundPage.current.liveView = canvas.imageView
